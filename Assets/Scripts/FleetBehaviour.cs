5a59a7eedd51d48d505bfc01f351e4f7c3cedc3e
Brett <babraham341@gmail.com>
2024-07-18 09:24:28 -0400

    Add tweening to fleet movement

--- Assets/Scripts/FleetBehaviour.cs
+++ Assets/Scripts/FleetBehaviour.cs
@@ -1,12 +1,22 @@
+using DG.Tweening;
 using UnityEngine;
+
 
 public class FleetBehaviour : MonoBehaviour
 {
     public PlanetBehaviour currentPlanet;
 
+    [SerializeField] float lookTweenDuration = 0.5f;
+    [SerializeField] float moveTweenDuration = 1f;
+
     public void MoveFleetToPlanetPosition(PlanetBehaviour targetPlanet)
     {
         currentPlanet = targetPlanet;
-        transform.position = targetPlanet.transform.position;
+        Transform targetTransform = targetPlanet.transform;
+
+        Sequence moveSequence = DOTween.Sequence();
+
+        moveSequence.Append(transform.DOLookAt(targetTransform.position, lookTweenDuration));
+        moveSequence.Append(transform.DOMove(targetPlanet.transform.position, moveTweenDuration, false).SetEase(Ease.InOutExpo));
     }
 }
