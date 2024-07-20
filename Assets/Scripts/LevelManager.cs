207c27becf2e1c45e7c3ab90c5973b3aadcbf9d1
Brett <babraham341@gmail.com>
2024-07-20 11:47:02 -0400

    Create level manager to handle dynamic transform and main camera

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType(typeof(LevelManager)) as LevelManager;
+
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    private static LevelManager instance;
+
+    public Transform DynamicTransform { get; private set; }
+    public Camera MainCamera { get; private set; }
+
+    private void Awake() {
+        CreateDynamicTransform();
+
+        MainCamera = Camera.main;
+    }
+
+    private void CreateDynamicTransform()
+    {
+        GameObject dynamicGameObject = new() { name = "_Dynamic" };
+        DynamicTransform = dynamicGameObject.transform;
+    }
+}
