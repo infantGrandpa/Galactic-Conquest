e9ecfbab4b0e0686b72f774ea0208f69fb48dd36
Brett <babraham341@gmail.com>
2024-07-20 11:46:17 -0400

    Automatically add planets to the movementmanager onenable

--- Assets/Scripts/PlanetBehaviour.cs
+++ Assets/Scripts/PlanetBehaviour.cs
@@ -5,6 +5,21 @@ public class PlanetBehaviour : MonoBehaviour
 {
     public Transform fleetSlotTransform;
 
+    private void OnEnable()
+    {
+        MovementManager.Instance.planets.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        if (MovementManager.Instance == null)
+        {
+            return;
+        }
+
+        MovementManager.Instance.planets.Remove(this);
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.cyan;
