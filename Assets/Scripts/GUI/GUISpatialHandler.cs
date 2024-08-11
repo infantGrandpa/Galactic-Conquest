81096ff5ce9d6461726101a362239a6200183375
Brett <babraham341@gmail.com>
2024-08-11 12:21:32 -0400

    Fix UI bug where spatial elements had scale greater than 1

--- Assets/Scripts/GUI/GUISpatialHandler.cs
+++ Assets/Scripts/GUI/GUISpatialHandler.cs
@@ -15,11 +15,13 @@ namespace Abraham.GalacticConquest
             dynamicUITransform = dynamicGameObject.transform;
             dynamicUITransform.SetParent(transform);
             dynamicUITransform.localPosition = Vector3.zero;
+            dynamicUITransform.localScale = Vector3.one;
         }
 
         public void AddUIElement(Transform transformToAdd)
         {
             transformToAdd.SetParent(dynamicUITransform);
+            transformToAdd.localScale = Vector3.one;
         }
     }
 }
