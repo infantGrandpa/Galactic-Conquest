3233c2fad990cada021a4b4db4cbc5d443d5f091
Brett <babraham341@gmail.com>
2024-08-17 12:30:33 -0400

    Add offset for action list to position it next to the planet rather than on top of it

--- Assets/Scripts/GUI/GUIActionListHandler.cs
+++ Assets/Scripts/GUI/GUIActionListHandler.cs
@@ -8,14 +8,35 @@ namespace Abraham.GalacticConquest.GUI
     public class GUIActionListHandler : MonoBehaviour
     {
         [SerializeField] TMP_Text header;
+        [SerializeField] Vector2 positionOffset;
+
+        RectTransform rectTransform;
+
+        PlanetBehaviour currentPlanet;
 
         void Awake()
         {
+            GetComponents();
             HideActionList();
+        }
+
+        void GetComponents()
+        {
+            rectTransform = GetComponent<RectTransform>();
         }
 
         public void ShowActionList(PlanetBehaviour planet)
         {
+            if (rectTransform == null) {
+                GetComponents();
+            }
+
+            currentPlanet = planet;
+
+            Vector2 canvasPosition = GUIManager.Instance.mainCanvas.WorldToCanvasPosition(planet.transform.position, GUIManager.Instance.mainCamera);
+            Vector2 finalPosition = canvasPosition + positionOffset;
+            rectTransform.anchoredPosition = finalPosition;
+            
             gameObject.SetActive(true);
             header.text = planet.planet.planetName;
 
@@ -23,7 +44,17 @@ namespace Abraham.GalacticConquest.GUI
 
         public void HideActionList()
         {
+            currentPlanet = null;
             gameObject.SetActive(false);
+        }
+
+        [ContextMenu("Recalculate")]
+        public void RecalculatePosition()
+        {
+            if (currentPlanet == null) {
+                Debug.LogWarning("No active planet.");
+            }
+            ShowActionList(currentPlanet);
         }
     }
 }
