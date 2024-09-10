d58260a6f0298f0d73acee59d2579f4b1d11b996
Brett <babraham341@gmail.com>
2024-09-10 17:07:34 -0400

    Show trait names and action point values in info box

--- Assets/Scripts/GUI/GUIInfoBoxHandler.cs
+++ Assets/Scripts/GUI/GUIInfoBoxHandler.cs
@@ -1,4 +1,6 @@
 using System;
+using Abraham.GalacticConquest.ActionPoints;
+using Abraham.GalacticConquest.Traits;
 using TMPro;
 using UnityEngine;
 
@@ -17,8 +19,41 @@ namespace Abraham.GalacticConquest.GUI
 
         public void ShowInfoBox(GameObject target)
         {
+            GetTraitInfo(target);
+            GetActionPointInfo(target);
+
             gameObject.SetActive(true);
-            Debug.Log("Showing Info box for " + target.name, this);
+        }
+
+        void GetTraitInfo(GameObject target)
+        {
+            TraitHandler targetTraitHandler = target.GetComponent<TraitHandler>();
+            if (targetTraitHandler == null) {
+                Debug.LogWarning("GUIInfoBoxHandler ShowInfoBox(): Target " + target.name + " does not have a trait handler.", this);
+                return;
+            }
+
+            string testString = "";
+
+            foreach (Trait thisTrait in targetTraitHandler.traits) {
+                testString += thisTrait.traitName + "\n";
+            }
+
+            descText.text = testString;
+        }
+
+        void GetActionPointInfo(GameObject target)
+        {
+            ActionPointAdjuster actionPointAdjuster = target.GetComponent<ActionPointAdjuster>();
+            if (actionPointAdjuster == null) {
+                Debug.LogWarning("GUIInfoBoxHandler GetActionPointInfo(): Target " + target.name + " does not have an action point adjuster.", this);
+                return;
+            }
+
+            int apValue = actionPointAdjuster.TotalApPerTurn;
+            string apString = apValue < 0 ? apValue.ToString() : "+" + apValue.ToString();
+
+            apPerTurnText.text = apString;
         }
 
         public void HideInfoBox()
