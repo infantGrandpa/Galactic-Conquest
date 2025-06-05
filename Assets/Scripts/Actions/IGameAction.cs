03c3f1b826494a429ed56bc73f31e1bed1a3edaa
Brett Abraham <babraham341@gmail.com>
2025-06-05 17:34:21 -0400

    Expand on TODO for the ActionResult concept

--- Assets/Scripts/Actions/IGameAction.cs
+++ Assets/Scripts/Actions/IGameAction.cs
@@ -4,7 +4,8 @@ namespace Abraham.GalacticConquest.Actions
     {
         int GetActionPointCost();
         bool CanExecuteAction();
-        //TODO: Instead of returning a bool, return an ActionResult class that provides details on the result 
+        // TODO: Instead of returning a bool, return an ActionResult class that provides details on the result 
+        //  This would include a message, the AP cost, if it was a success, and possibly more. 
         bool ExecuteAction();
         bool UndoAction();
     }
