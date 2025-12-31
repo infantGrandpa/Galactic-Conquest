9fbf08507a7347a0a411ade80766ec2b9fbdf733
Brett Abraham <babraham341@gmail.com>
2025-12-31 11:01:07 -0500

    Refactor MoveAction.cs to use ActionResults

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -38,31 +38,41 @@ namespace Abraham.GalacticConquest.Actions
         {
             if (!_moveableObject)
             {
-                return false;
+                Result = ActionResult.Failure(GetActionTypeName(), "A moveable object is not selected.");
+                return Result.WasSuccessful;
             }
 
             if (!_targetPlanet)
             {
-                //Didn't click on a planet. Cancel.
-                return false;
+                Result = ActionResult.Failure(GetActionTypeName(), "The user didn't click on a planet.");
+                return Result.WasSuccessful;
             }
             
             bool canMove = _moveableObject.CanMoveToTarget(_targetPlanet);
             if (!canMove)
             {
-                //Moveable object already at planet. Cancel.
-                return false;
+                Result = ActionResult.Failure(GetActionTypeName(), $"Move prevented by moveable object.");
+                return Result.WasSuccessful;
             }
             
             int apCost = GetActionPointCost();
-            return ActionPointManager.Instance.CanPerformAction(apCost);
+            if (!ActionPointManager.Instance.CanPerformAction(apCost))
+            {
+                string msg = $"This move action requires {apCost} AP.";
+                GUIManager.Instance.AddActionLogMessage(msg);
+                
+                Result = ActionResult.Failure(GetActionTypeName(), msg);
+                return Result.WasSuccessful;
+            }
+
+            return true;
         }
 
         public override bool ExecuteAction()
         {
             if (!CanExecuteAction())
             {
-                return false;
+                return Result.WasSuccessful;
             }
 
             _startingPlanet = _moveableObject.currentPlanet;
@@ -71,15 +81,18 @@ namespace Abraham.GalacticConquest.Actions
             bool moveSuccessful = _moveableObject.MoveToPlanet(_targetPlanet);
             if (!moveSuccessful)
             {
-                //Move cancelled by moveable object.
-                return false;
+                Result = ActionResult.Failure(GetActionTypeName(), $"Move cancelled by moveable object.");
+                return Result.WasSuccessful;
             }
 
             int apCost = GetActionPointCost();
             ActionPointManager.Instance.DecreaseActionPoints(apCost);
-            GUIManager.Instance.AddActionLogMessage($"Moved {_moveableObject.gameObject.name} to {_targetPlanet.PlanetInfo.myName}.",
-                GetActionPointCost() * -1);
-            return true;
+
+            string msg = $"Moved {_moveableObject.gameObject.name} to {_targetPlanet.PlanetInfo.myName}.";
+            GUIManager.Instance.AddActionLogMessage(msg, apCost * -1);
+
+            Result = ActionResult.Success(GetActionTypeName(), apCost, msg);
+            return Result.WasSuccessful;
         }
 
         public override bool UndoAction()
