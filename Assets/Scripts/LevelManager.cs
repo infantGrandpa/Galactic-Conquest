233c24ba1d63e2be203fa800d1f581adb18e482f
Brett Abraham <babraham341@gmail.com>
2026-01-01 11:47:09 -0500

    Add dev controls to delete saves

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -50,6 +50,17 @@ namespace Abraham.GalacticConquest
                     //Restart level
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 }
+                
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    //Delete All Saves
+                    ES3Settings settings = new ES3Settings();
+                    foreach (var fileName in ES3.GetFiles(settings))
+                    {
+                        ES3.DeleteFile(fileName, settings);
+                        Debug.Log($"{fileName} deleted.");
+                    }
+                }
             }
         }
 
